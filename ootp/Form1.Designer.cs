b38d86f802080009010c4a885254e565217ebafd
angelina <afonina.01@inbox.ru>
2021-02-28 19:17:18 +0300

    loadfirstlab

---
+++ ootp/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ellipse = new System.Windows.Forms.Button();
+            this.rectangle = new System.Windows.Forms.Button();
+            this.Polygon = new System.Windows.Forms.Button();
+            this.Lines = new System.Windows.Forms.Button();
+            this.Line = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ellipse
+            // 
+            this.ellipse.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ellipse.Location = new System.Drawing.Point(12, 12);
+            this.ellipse.Name = "ellipse";
+            this.ellipse.Size = new System.Drawing.Size(114, 33);
+            this.ellipse.TabIndex = 0;
+            this.ellipse.Text = "Эллипс";
+            this.ellipse.UseVisualStyleBackColor = false;
+            this.ellipse.Click += new System.EventHandler(this.ellipse_Click);
+            // 
+            // rectangle
+            // 
+            this.rectangle.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rectangle.Location = new System.Drawing.Point(311, 12);
+            this.rectangle.Name = "rectangle";
+            this.rectangle.Size = new System.Drawing.Size(165, 33);
+            this.rectangle.TabIndex = 2;
+            this.rectangle.Text = "Прямоугольник";
+            this.rectangle.UseVisualStyleBackColor = false;
+            this.rectangle.Click += new System.EventHandler(this.rectangle_Click);
+            // 
+            // Polygon
+            // 
+            this.Polygon.BackColor = System.Drawing.SystemColors.Info;
+            this.Polygon.Location = new System.Drawing.Point(495, 12);
+            this.Polygon.Name = "Polygon";
+            this.Polygon.Size = new System.Drawing.Size(145, 33);
+            this.Polygon.TabIndex = 3;
+            this.Polygon.Text = "Многоульник";
+            this.Polygon.UseVisualStyleBackColor = false;
+            this.Polygon.Click += new System.EventHandler(this.Polygon_Click);
+            // 
+            // Lines
+            // 
+            this.Lines.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Lines.Location = new System.Drawing.Point(672, 12);
+            this.Lines.Name = "Lines";
+            this.Lines.Size = new System.Drawing.Size(114, 33);
+            this.Lines.TabIndex = 4;
+            this.Lines.Text = "Ломаная";
+            this.Lines.UseVisualStyleBackColor = false;
+            this.Lines.Click += new System.EventHandler(this.Lines_Click);
+            // 
+            // Line
+            // 
+            this.Line.BackColor = System.Drawing.SystemColors.Info;
+            this.Line.Location = new System.Drawing.Point(168, 12);
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(114, 33);
+            this.Line.TabIndex = 5;
+            this.Line.Text = "Прямая";
+            this.Line.UseVisualStyleBackColor = false;
+            this.Line.Click += new System.EventHandler(this.Line_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 450);
+            this.Controls.Add(this.Line);
+            this.Controls.Add(this.Lines);
+            this.Controls.Add(this.Polygon);
+            this.Controls.Add(this.rectangle);
+            this.Controls.Add(this.ellipse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ellipse;
+        private System.Windows.Forms.Button rectangle;
+        private System.Windows.Forms.Button Polygon;
+        private System.Windows.Forms.Button Lines;
+        private System.Windows.Forms.Button Line;
+    }
+}
+
---
+++ ootp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ellipse_Click(object sender, EventArgs e)
+        {
+            Pen pn = new Pen(Color.Blue, 3); 
+            Graphics g = CreateGraphics();
+            g.DrawEllipse(pn, 20, 50, 70, 35);
+        }
+
+        private void Line_Click_1(object sender, EventArgs e)
+        {
+            Pen pn = new Pen(Color.Red, 3);
+            Graphics g = CreateGraphics();
+            g.DrawLine(pn, 145, 45, 145, 95);
+        }
+
+        private void rectangle_Click(object sender, EventArgs e)
+        {
+            Pen pn = new Pen(Color.Blue, 3);
+            Graphics g = CreateGraphics();
+            g.DrawRectangle(pn, 225, 45, 70, 45);
+        }
+
+        private void Polygon_Click(object sender, EventArgs e)
+        {
+            Pen pn = new Pen(Color.Red, 3);
+            Graphics g = CreateGraphics();
+            Point point1 = new Point(370, 45);
+            Point point2 = new Point(405, 75);
+            Point point3 = new Point(390, 105);
+            Point point4 = new Point(350, 105);
+            Point point5 = new Point(335, 75);
+            Point[] Points =
+                     {
+                 point1,
+                 point2,
+                 point3,
+                 point4,
+                 point5
+             };
+            g.DrawPolygon(pn, Points);
+        }
+
+        private void Lines_Click(object sender, EventArgs e)
+        {
+            Pen pn = new Pen(Color.Blue, 3);
+            Graphics g = CreateGraphics();
+            Point[] points =
+            {
+                 new Point(500, 45),
+                 new Point(480, 75),
+                 new Point(490, 70),
+                 new Point(470, 105)
+             };
+            g.DrawLines(pn, points);
+        }
+
+        
+    }
+}
